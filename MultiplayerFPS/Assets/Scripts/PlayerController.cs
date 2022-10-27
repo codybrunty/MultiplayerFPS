a6d8ed63bbd0f19495d16d9f8e92729b73ab936b
cody brunty <codybrunty@gmail.com>
2022-10-27 15:19:49 -0700

    Running and Jumping
    Running and Jumping added to player controller

--- MultiplayerFPS/Assets/Scripts/PlayerController.cs
+++ MultiplayerFPS/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour{
 
     [Header("References")]
     [SerializeField] private Transform viewPoint;
+    private Camera cam;
+    private CharacterController characterController;
+    [SerializeField] private Transform groundCheckPoint;
     [Header("Mouse Settings")]
     public bool invertedLook;
     public float mouseSensitivity;
@@ -14,11 +17,20 @@ public class PlayerController : MonoBehaviour{
     private float verticalRotationStore;
     [Header("Move Settings")]
     public float moveSpeed;
+    public float runSpeed;
+    private float acitveMoveSpeed;
     private Vector3 moveDirection;
     private Vector3 movement;
+    [Header("Jump Settings")]
+    public float gravityMod;
+    public float jumpForce;
+    private bool isGrounded;
+    public LayerMask groundLayers;
 
     private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+        cam = Camera.main;
     }
 
     private void Update() {
@@ -26,12 +38,45 @@ public class PlayerController : MonoBehaviour{
         PlayerMovement();
     }
 
-    private void PlayerMovement() {
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical"));
-        movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized;
-        transform.position += movement * moveSpeed * Time.deltaTime;
+    private void LateUpdate() {
+        SetCameraPosition();
+        SetCameraRotation();
     }
 
+    #region Translation
+    private void PlayerMovement() {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        acitveMoveSpeed = GetSpeed();
+        float yVelocity = movement.y;
+        movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized * acitveMoveSpeed;
+        movement.y = yVelocity;
+        if (characterController.isGrounded) {
+            movement.y = 0f;
+        }
+
+        GroundCheck();
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            movement.y = jumpForce;
+        }
+
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
+        characterController.Move(movement * Time.deltaTime);
+    }
+
+    private void GroundCheck() {
+        isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, .25f, groundLayers);
+    }
+
+    private float GetSpeed() {
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            return runSpeed;
+        }
+        return moveSpeed;
+    }
+
+    #endregion
+
+    #region Rotation
     private void PlayerRotation() {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
         transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z));
@@ -44,4 +89,15 @@ public class PlayerController : MonoBehaviour{
             viewPoint.rotation = Quaternion.Euler(new Vector3(-verticalRotationStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z));
         }
     }
+    #endregion
+
+    #region Camera
+    private void SetCameraPosition() {
+        cam.transform.position = viewPoint.position;
+    }
+    private void SetCameraRotation() {
+        cam.transform.rotation = viewPoint.rotation;
+    }
+    #endregion
+
 }
